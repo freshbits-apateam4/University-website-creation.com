5b783586d717f31313c7e5bdf94cef1304691d08
freshbits-apateam4 <144349104+freshbits-apateam4@users.noreply.github.com>
2024-04-04 12:58:11 +0530

    Add files via upload

---
+++ college/form-handler.php
@@ -0,0 +1,26 @@
+<?php
+$name = $_POST['name']
+$visitor_email = $_POST['email']
+$subject = $_POST['subject']
+$message = $_POST['message']
+
+$email_from = 'MAS@gmail.com';
+
+$email_subject = 'New Form Submission';
+
+$email_body = "User name: $name.\n".
+               "User Email: $visitor_email.\n".
+               "Subject: $subject.\n".
+                "User Message: $message.\n";
+
+$to = 'hariprasath121982@gmail.com';
+
+$headers = "From: $email_from \r\n";
+
+$headers .= "Replay-To: $visitor_email \r\n";
+
+
+mail($to,$email_subject,$email_body,$headers);
+
+header("location: contact.html");
+?>
